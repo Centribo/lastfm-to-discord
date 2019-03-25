c2bf60af3f7dd886ef41a6d9001b1dcc5d740549
Adam Leung <centribo@gmail.com>
2019-03-25 17:21:03 -0400

    Got last.fm, logging into Discord, and reading from config working

--- index.js
+++ index.js
@@ -1,2 +1,54 @@
-console.log("Hello World");
-var a = 1;
+var fs = require('fs');
+var http = require('http');
+var Discord = require('discord.js');
+var client = new Discord.Client();
+var configData = fs.readFileSync('config.json');
+var config = JSON.parse(configData);
+var updateInterval = config.interval;
+var lastfmUsername = config.lastfmUsername;
+var lastfmApiKey = config.lastfmApiKey;
+var discordToken = config.discordToken;
+client.on('ready', function () {
+    console.log(client.user.tag);
+});
+client.login(discordToken);
+http.get("http://ws.audioscrobbler.com/2.0/"
+    + "?method=" + "user.getrecenttracks"
+    + "&user=" + lastfmUsername
+    + "&api_key=" + lastfmApiKey
+    + "&limit=" + "1"
+    + "&format=json", function (resp) {
+    var data = '';
+    resp.on('data', function (chunk) {
+        data += chunk;
+    });
+    resp.on('end', function () {
+        var dataJSON = JSON.parse(data);
+        updateDiscordStatus(dataJSON.recenttracks.track[0]);
+    });
+}).on('error', function (err) {
+    console.log("Error:" + err.message);
+});
+function updateDiscordStatus(trackData) {
+    var isNowPlaying = trackData["@attr"].nowplaying;
+    var artist = trackData.artist["#text"];
+    var album = trackData.album["#text"];
+    var song = trackData.name;
+    if (isNowPlaying) {
+    }
+    else {
+    }
+}
+// https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
+// 	let data = '';
+// 	// A chunk of data has been recieved.
+// 	resp.on('data', (chunk) => {
+// 		data += chunk;
+// 	});
+// 	// The whole response has been received. Print out the result.
+// 	resp.on('end', () => {
+// 		console.log(JSON.parse(data).explanation);
+// 	});
+// }).on("error", (err) => {
+// 	console.log("Error: " + err.message);
+// });
--- index.ts
+++ index.ts
@@ -1,3 +1,72 @@
-console.log("Hello World");
+const fs    = require('fs');
+const http = require('http');
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-var a:number = 1;
+let configData            = fs.readFileSync('config.json');
+let config                = JSON.parse(configData);
+let updateInterval:number = config.interval;
+let lastfmUsername:string = config.lastfmUsername;
+let lastfmApiKey:string   = config.lastfmApiKey;
+let discordToken:string   = config.discordToken;
+
+client.on('ready', () => {
+	console.log(client.user.tag);
+});
+
+client.login(discordToken);
+
+http.get(
+	"http://ws.audioscrobbler.com/2.0/"
+	+ "?method=" + "user.getrecenttracks"
+	+ "&user=" + lastfmUsername
+	+ "&api_key=" + lastfmApiKey
+	+ "&limit=" + "1"
+	+ "&format=json",
+	
+	(resp) => {
+		let data:string = '';
+		
+		resp.on('data', (chunk) =>{
+			data += chunk;
+		});
+		
+		resp.on('end', ()=> {
+			let dataJSON = JSON.parse(data);
+			
+			updateDiscordStatus(dataJSON.recenttracks.track[0]);
+		});
+		
+}).on('error', (err) =>{
+		console.log("Error:" + err.message);
+});
+
+function updateDiscordStatus(trackData){
+	let isNowPlaying:boolean = trackData["@attr"].nowplaying;
+	let artist:string = trackData.artist["#text"];
+	let album:string = trackData.album["#text"];
+	let song:string = trackData.name;
+
+	if(isNowPlaying){
+
+	} else {
+
+	}
+}
+
+// https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
+// 	let data = '';
+	
+// 	// A chunk of data has been recieved.
+// 	resp.on('data', (chunk) => {
+// 		data += chunk;
+// 	});
+	
+// 	// The whole response has been received. Print out the result.
+// 	resp.on('end', () => {
+// 		console.log(JSON.parse(data).explanation);
+// 	});
+	
+// }).on("error", (err) => {
+// 	console.log("Error: " + err.message);
+// });
